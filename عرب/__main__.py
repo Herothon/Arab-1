5661dc06cef42c612d36c5d188162c1f1749f2af
cdctop <129498284+mustefe@users.noreply.github.com>
2023-04-17 19:48:09 +0300

    Update and rename Arab/__main__.py to عرب/__main__.py

--- عرب/__main__.py
+++ عرب/__main__.py
@@ -6,12 +6,12 @@ from .Config import Config
 from .core.logger import logging
 from .core.session import iqthon
 from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
-LOGS = logging.getLogger("تليثون العرب")
+LOGS = logging.getLogger("تليثون الجمهورية")
 print(Arab.__copyright__)
 print("المرخصة بموجب شروط " + Arab.__license__)
 cmdhr = Config.COMMAND_HAND_LER
 try:
-    LOGS.info("بدء تنزيل تليثون العرب")
+    LOGS.info("بدء تنزيل تليثون الجمهورية")
     iqthon.loop.run_until_complete(setup_bot())
     LOGS.info("بدء تشغيل البوت")
 except Exception as e:
@@ -44,7 +44,7 @@ async def startup_process():
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print(f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/iqthon ")
+    print(f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/CDCTOP ")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
@@ -61,7 +61,7 @@ async def startup_process():
     return
 
 
-iqthon.loop.run_until_complete(startup_process())    
+Heathon.loop.run_until_complete(startup_process())    
 if len(sys.argv) not in (1, 3, 4):
     iqthon.disconnect()
 elif not Catcheck.sucess:
@@ -69,6 +69,6 @@ elif not Catcheck.sucess:
         HEROKU_APP.restart()
 else:
     try:
-        iqthon.run_until_disconnected()
+        heathon.run_until_disconnected()
     except ConnectionError:
         pass
