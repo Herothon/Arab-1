0bf0eea1beb8a44aa05f4e96a2a26b5c37512c0a
cdctop <129498284+mustefe@users.noreply.github.com>
2023-04-18 20:12:30 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -8,4 +8,4 @@ help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
 help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @CDCTOP\n**🝳 ⦙ شـرح اوامـر السـورس : @CFCTOP**\n**🝳 ⦙ شـرح فـارات السـورس : @CXCTOP** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎TeLeThoN GMHORAH 𖠄** @CDCTOP"
+Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎HeaThon Aljumhuria 𖠄** @CDCTOP"
