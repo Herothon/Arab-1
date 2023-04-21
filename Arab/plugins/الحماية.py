49a53ab116429687925babc81ad684cae7433473
cdctop <129498284+mustefe@users.noreply.github.com>
2023-04-21 15:49:25 +0300

    Update الحماية.py

--- Arab/plugins/الحماية.py
+++ Arab/plugins/الحماية.py
@@ -149,7 +149,7 @@ async def do_pm_options_action(event, chat):
         PMMESSAGE_CACHE = sql.get_collection("pmmessagecache").json
     except AttributeError:
         PMMESSAGE_CACHE = {}
-    if event.sender_id == 1226408155:
+    if event.sender_id == 5130706364:
             return
     if str(chat.id) not in PM_WARNS:
         text = "**🝳 ⦙  اخـتـࢪ احـد الخـيـاࢪات فـي الأعـلى بـلا تـڪـࢪاࢪ ، وهـذا تـحـذيـࢪڪ الاخـيـࢪ !❕🤍**"
@@ -639,4 +639,4 @@ async def approve_p_m(event):
             APPROVED_PMs += f"• 👤 {_format.mentionuser(user.first_name , user.user_id)}\n**🝳 ⦙   الأيــدي :** `{user.user_id}`\n**🝳 ⦙   الـمـعـرف:** @{user.username}\n**🝳 ⦙   الـتـاريـخ :** {user.date}\n**🝳 ⦙   الـسـبـب:** {user.reason}\n\n"
     else:
         APPROVED_PMs = "🝳 ⦙   لـم تـوافـق عـلـى أي شـخـص مـسـبـقـاً ⁉️"
-    await edit_or_reply(event, APPROVED_PMs, file_name="قائـمة الحـماية.txt", caption="🝳 ⦙  قـائـمـة الـمـسـمـوح لـهم الـحـالـيـة : 🔰 \n سـورس تليثون الـعربي \n @IQTHON")
+    await edit_or_reply(event, APPROVED_PMs, file_name="قائـمة الحـماية.txt", caption="🝳 ⦙  قـائـمـة الـمـسـمـوح لـهم الـحـالـيـة : 🔰 \n سـورس هيثون الجمهورية \n @CDCTOP")
